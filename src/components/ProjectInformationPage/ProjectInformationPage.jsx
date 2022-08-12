371556f3dfa55d34c0060bc3187548350a4cfbe4
Carlos Kelley <carloskelley@protonmail.com>
2022-08-11 19:44:47 -0500

    condit. lang. add reducer and toggle. later will pull from db

--- src/components/ProjectInformationPage/ProjectInformationPage.jsx
+++ src/components/ProjectInformationPage/ProjectInformationPage.jsx
@@ -13,59 +13,127 @@ function ProjectInformationPage(props) {
 
   return (
     <div>
-      <h2>{heading}</h2>
-      {/* create a questionnaire for painting houses */}
-      <p>
-        <strong>Client Name:</strong>
-      </p>
-      <p>
-        <strong>Client Address:</strong>
-      </p>
-      <p>
-        <strong>City:</strong>
-      </p>
-      <p>
-        <strong>State:</strong>
-      </p>
-      <p>
-        <strong>Zip Code:</strong>
-      </p>
-      <p>
-        <strong>Primary Phone:</strong>
-      </p>
-      <p>
-        <strong>Email:</strong>
-      </p>
-      <p>
-        <strong>Paint ID:</strong>
-      </p>
-      <p>
-        <strong>Project Location ID:</strong>
-      </p>
-      <p>
-        <strong>Project Status ID:</strong>
-      </p>
-      <p>
-        <strong>Language ID:</strong>
-      </p>
-      <p>
-        <strong>Photo:</strong>
-      </p>
-      <p>
-        <strong>Estimate:</strong>
-      </p>
-      <p>
-        <strong>Estimated Startdate:</strong>
-      </p>
-      <p>
-        <strong>Notes:</strong>
-      </p>
-      <p>
-        <strong>Downpayment Amount:</strong>
-      </p>
-      <p>
-        <strong>Current Date:</strong>
-      </p>
+      {store.language === "english" ? (
+        <>
+          <h2>{heading}</h2>
+          <p>
+            <strong>Client Name:</strong>
+          </p>
+          <p>
+            <strong>Client Address:</strong>
+          </p>
+          <p>
+            <strong>City:</strong>
+          </p>
+          <p>
+            <strong>State:</strong>
+          </p>
+          <p>
+            <strong>Zip Code:</strong>
+          </p>
+          <p>
+            <strong>Primary Phone:</strong>
+          </p>
+          <p>
+            <strong>Email:</strong>
+          </p>
+          <p>
+            <strong>Paint ID:</strong>
+          </p>
+          <p>
+            <strong>Project Location ID:</strong>
+          </p>
+          <p>
+            <strong>Project Status ID:</strong>
+          </p>
+          <p>
+            <strong>Language ID:</strong>
+          </p>
+          <p>
+            <strong>Photo:</strong>
+          </p>
+          <p>
+            <strong>Estimate:</strong>
+          </p>
+          <p>
+            <strong>Estimated Startdate:</strong>
+          </p>
+          <p>
+            <strong>Notes:</strong>
+          </p>
+          <p>
+            <strong>Downpayment Amount:</strong>
+          </p>
+          <p>
+            <strong>Current Date:</strong>
+          </p>
+        </>
+      ) : (
+        <>
+          <h2>{heading}</h2>
+          <p>
+            <strong>Nombre del Cliente:</strong>
+          </p>
+          <p>
+            <strong>
+              Dirección del Cliente:
+            </strong>
+          </p>
+          <p>
+            <strong>Ciudad:</strong>
+          </p>
+          <p>
+            <strong>Estado:</strong>
+          </p>
+          <p>
+            <strong>Código Postal:</strong>
+          </p>
+          <p>
+            <strong>Teléfono Principal:</strong>
+          </p>
+          <p>
+            <strong>Email:</strong>
+          </p>
+          <p>
+            <strong>ID de Pintura:</strong>
+          </p>
+          <p>
+            <strong>
+              ID de Ubicación del Proyecto:
+            </strong>
+          </p>
+          <p>
+            <strong>
+              ID de Estatus del Proyecto:
+            </strong>
+          </p>
+          <p>
+            <strong>ID de Idioma:</strong>
+          </p>
+          <p>
+            <strong>Foto:</strong>
+          </p>
+          <p>
+            <strong>Estimado:</strong>
+          </p>
+          <p>
+            <strong>
+              Fecha de Inicio Estimada:
+            </strong>
+          </p>
+          <p>
+            <strong>Notas:</strong>
+          </p>
+          <p>
+            <strong>
+              Monto de Pago Inicial:
+            </strong>
+          </p>
+          <p>
+            <strong>Fecha Actual:</strong>
+          </p>
+        </>
+      )}
     </div>
   );
 }
