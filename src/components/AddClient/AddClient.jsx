4537d96c4303110a2197f7e8576443d42952e885
ahowes83 <a_howes83@yahoo.com>
2022-08-21 15:13:32 -0500

    testing out finalform

--- src/components/AddClient/AddClient.jsx
+++ src/components/AddClient/AddClient.jsx
@@ -1,101 +1,201 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { render } from 'react-dom';
 import {useDispatch, useSelector} from 'react-redux';
-import './AddProject.css';
+import { Form, Field } from 'react-final-form';
+import './AddClient.css';
 
 // CUSTOM COMPONENTS
 import RegisterForm from '../RegisterForm/RegisterForm';
 
-class AddClient extends React.Component {
- // const [lorem, ipsum] = useState('');
-  const dispatch = useDispatch();
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-  // useEffect(() => {
-  // }, []);
+const onSubmit = async values => {
+  await sleep(300)
+  window.alert(JSON.stringify(values, 0, 2))
+}
 
-  const history = useHistory();
-  const inputData = {};
-
-  setValue (event) {
-    inputData[newOrExisting] = event.target.value;
-    console.log(event.target.value)
-  }
-
-  let clientType = <></>;
-
-  setClientType(event) {
-    switch (event.target.value){
-      case "Homeowner":
-        clientType = <></>;
-        break;
-      case "Business Owner":
-        clientType = <input id="businessName" placeholder="Business Name"></input>
-        <label for="businessName">Business Name</label>;
-        break;
-      case "Contractor/Developer":
-        clientType = <input id="repName" placeholder="Representative Name"></input>
-        <label for="repName">Representative Name</label> <input id="businessName" placeholder="Business Name"></input>
-        <label for="businessName">Business Name</label>;
-        break;
-      case "Realtor/Designer":
-        clientType = <input id="repName" placeholder="Representative Name"></input>
-        <label for="repName">Representative Name</label> <input id="businessName" placeholder="Business Name"></input>
-        <label for="businessName">Business Name</label>;
-        break;
-      case "Landlord/Property Owner":
-        clientType = <input id="repName" placeholder="Representative Name"></input>
-        <label for="repName">Representative Name</label> <input id="businessName" placeholder="Business Name"></input>
-        <label for="businessName">Business Name</label>;
-        break;
-      default:
-        clientType = <></>;
-        break;
+const Error = ({ name }) => (
+  <Field name={name} subscription={{ error: true, touched: true }}>
+    {({ meta: { error, touched } }) =>
+      error && touched ? <span>{error}</span> : null
     }
-  }
+  </Field>
+)
 
-  // const loremPush = () => {
-  //   history.push(`/`);
-  // };
-  render() {
-    return
-      <div className="container">
-        <h2>Customer Info</h2>
-        <div className="radioBox" onChange={event => this.setValue(event)}>
-          <input type="radio" value="New" name="newOrExistingCustomer" /> New
-          <input type="radio" value="Existing" name="newOrExistingCustomer" /> Existing
+const Condition = ({ when, is, children }) => (
+  <Field name={when} subscription={{ value: true }}>
+    {({ input: { value } }) => (value === is ? children : null)}
+  </Field>
+)
+
+const AddClient = () => (
+	<Form
+    onSubmit={onSubmit}
+    validate={values => {
+      const errors = {}
+      if (!values.firstName) {
+        errors.firstName = 'Required'
+      }
+      if (!values.reception) {
+        errors.reception = 'Required'
+      }
+      if (values.reception === 'delivery') {
+        if (!values.street) {
+          errors.street = 'Required'
+        }
+      } else if (values.reception === 'pickup') {
+        if (!values.pickupTime) {
+          errors.pickupTime = 'Required'
+        }
+      }
+      return errors
+    }}
+  >
+	{({ handleSubmit, form, submitting, pristine, values }) => (
+    <form onSubmit={handleSubmit}>
+      <label>New or Existing Customer?</label>
+      <div>
+        <label>
+          <Field
+            name="newOrExisting"
+            component="input"
+            type="radio"
+            value="new"
+          />{' '}
+          New
+        </label>
+        <label>
+          <Field
+            name="newOrExisting"
+            component="input"
+            type="radio"
+            value="existing"
+          />{' '}
+          Existing
+        </label>
+      </div>
+      <Condition when="newOrExisting" is="new">
+        <div>
+          <label>Client Type</label>
+          <Field name="clientType" component="select">
+            <option />
+            <option key={} value={homeowner}>
+              Homeowner
+            </option>
+            <option key={} value={businessOwner}>
+              Business Owner
+            </option>
+            <option key={} value={contractorDeveloper}>
+              Contractor/Developer
+            </option>
+            <option key={} value={realtorDesigner}>
+              Realtor/Designer
+            </option>
+            <option key={} value={LandlordPropertyOwner}>
+              Landlord/Property Owner
+            </option>
+          </Field>
+          <Error name="clientType" />
         </div>
-        <div className="dropdownSelector">
-          <select onChange={event => this.setClientType(event)}>
-            <option>Homeowner</option>
-            <option>Business Owner</option>
-            <option>Contractor/Developer</option>
-            <option>Realtor/Designer</option>
-            <option>Landlord/Property Owner</option>
-          </select>
+      </Condition>
+      <div id="customerTextInputs">
+        <div>
+          <label>Client Name</label>
+          <Field
+            name="clientName"
+            component="input"
+            type="text"
+            placeholder="Client Name"
+          />
+          <Error name="clientName" />
         </div>
-        <div id="customerTextInputs">
-          {/* <input id="repName" placeholder="Representative Name"></input>
-          <label for="repName">Representative Name</label>
-          <input id="businessName" placeholder="Business Name"></input>
-          <label for="businessName">Business Name</label> */}
-          {clientType}
-          <input id="clientName" placeholder="Client Name"></input>
-          <label for="clientName">Client Name</label>
-          <input id="decisionMakerName" placeholder="Decision-Maker Name"></input>
-          <label for="decisionMakerName">Decision-Maker Name</label>
-          <input type="email" id="email" placeholder="Email"></input>
-          <label for="email">Email</label>
-          <input type="tel" id="phone" placeholder="Phone"></input>
-          <label for="phone">Phone #</label>
-          <input id="address" placeholder="Address"></input>
-          <label for="address">Address</label>
-          <input id="yearBuilt" placeholder="Year Built"></input>
-          <label for="yearBuilt">Year Built</label>
-          <input id="notes" placeholder="Notes"></input>
-          <label for="notes">Notes</label>
+        <label>Is this the decision-maker?</label>
+        <div>
+          <label>
+            <Field
+              name="isDecisionMaker"
+              component="input"
+              type="radio"
+              value="yes"
+            />{' '}
+            Yes
+          </label>
+          <label>
+            <Field
+              name="isDecisionMaker"
+              component="input"
+              type="radio"
+              value="no"
+            />{' '}
+            No
+          </label>
+        </div>
+        <Condition when="isDecisionMaker" is="no">
+          <div>
+            <label>Decision Maker Name</label>
+            <Field
+              name="decisionMakerName"
+              component="input"
+              type="text"
+              placeholder="Decision Maker Name"
+            />
+            <Error name="decisionMakerName" />
+          </div>
+        </Condition>
+        <div>
+          <label>Email</label>
+          <Field
+            name="email"
+            component="input"
+            type="email"
+            placeholder="Email"
+          />
+          <Error name="email" />
+        </div>
+        <div>
+          <label>Phone #</label>
+          <Field
+            name="phone"
+            component="input"
+            type="tel"
+            placeholder="Phone #"
+          />
+          <Error name="phone" />
+        </div>
+        <div>
+          <label>Address</label>
+          <Field
+            name="address"
+            component="input"
+            type="text"
+            placeholder="Address"
+          />
+          <Error name="address" />
+        </div>
+        <div>
+          <label>Year Built</label>
+          <Field
+            name="yearBuilt"
+            component="input"
+            type="number"
+            placeholder="Year Built"
+          />
+          <Error name="yearBuilt" />
+        </div>
+        <div>
+          <label>Notes</label>
+          <Field
+            name="notes"
+            component="input"
+            type="text"
+            placeholder="Notes"
+          />
+          <Error name="notes" />
         </div>
       </div>
-  }
-}
+    </form>
+  )}
+);
 
 export default AddClient;
