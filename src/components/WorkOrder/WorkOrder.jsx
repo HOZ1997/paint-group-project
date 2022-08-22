584a9eff4747cc3e5e5eefefbc8fdf922bc013e5
Harrison Browning <harrisonbrowning1@gmail.com>
2022-08-21 21:16:43 -0500

    all required form fields created and linked to their corresponding functions to capture inputs. date captures still need work

--- src/components/WorkOrder/WorkOrder.jsx
+++ src/components/WorkOrder/WorkOrder.jsx
@@ -4,12 +4,11 @@ import {useSelector} from 'react-redux';
 function WorkOrder(props) {
 
   const store = useSelector((store) => store);
-  const [heading, setHeading] = useState('Functional Component');
 
   //hooks for inputs
   const [property, setProperty] = useState();
   const [building, setBuilding] = useState();
-  const [startDate, setStartDate] = useState();
+  const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState();
 
   //hooks for checkboxes
@@ -19,7 +18,19 @@ function WorkOrder(props) {
   const [checkedSpecialFeature, setCheckedSpecialFeature] = useState(false);
 
 
+  //------------------------------------------------------------------------------//
 
+
+  //functions for dropdown menus
+  const setPropertyType = (event) => {
+    console.log('you selected: ', event.target.value, ' for property type');
+    setProperty(event.target.value);
+  }
+
+  const setBuildingType = (event) => {
+    console.log('you selected: ', event.target.value, ' for building type');
+    setBuilding(event.target.value);
+  }
 
   //function for checbox change
   const handleInteriorCheckboxChange = (event) => {
@@ -42,13 +53,32 @@ function WorkOrder(props) {
     setCheckedSpecialFeature(!checkedSpecialFeature);
   }
 
+  //function for date change
+  const handleStartDateChange = () => {
+    console.log('you selected the start date', setStartDate);
+    setStartDate(event.target.value);
+  }
+
+
+  //function to capture all inputs
+  const handleSubmit = () => {
+    console.log('you submitted the form');
+    console.log('property: ', property);
+    console.log('building: ', building);
+    console.log('start date: ', startDate);
+    console.log('end date: ', endDate);
+    console.log('interior: ', checkedInterior);
+    console.log('exterior: ', checkedExterior);
+    console.log('cabinetry: ', checkedCabinetry);
+    console.log('special feature: ', checkedSpecialFeature);
+  }
+
   return (
     <div>
       <h2>Project Component</h2>
-        <div className='property_type'>
+        <div>
           <p>Property Type</p>
-
-          <select className='property_type'>
+          <select className='property_type' onChange={setPropertyType}>
             <option value='residential'>Residential</option>
             <option value='commercial'>Commercial</option>
             <option value='new_construction'>New Construction</option>
@@ -56,7 +86,7 @@ function WorkOrder(props) {
 
           <p>Bulding Type</p>
 
-          <select className='building_type'>
+          <select className='building_type' onChange={setBuildingType}>
             <option value='single_family_home'>Single Family Home</option>
             <option value='commercial_building'>Commercial Building</option>
             <option value='apartment'>Apartment</option>
@@ -100,6 +130,25 @@ function WorkOrder(props) {
             Special Feature
           </label>
 
+          <br/>
+          <br/>
+
+          <p>Desired Project Start Date</p>
+          <input
+          type='date'
+          value={startDate}
+          onChange={handleStartDateChange}
+          ></input>
+
+          <br/>
+          
+          <p>Desired Project End Date</p>
+          <input type='date'></input>
+
+        </div>
+
+        <div>
+          <button onClick={handleSubmit}>NEXT PAGE</button>
         </div>
     </div>
   );
