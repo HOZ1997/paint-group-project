3b28759e06d9e7a1c485b9bdec47df48a6dfea67
Carlos Kelley <carloskelley@protonmail.com>
2022-08-23 16:28:45 -0500

    labels are set

--- src/components/projectScope/projectScope.jsx
+++ src/components/projectScope/projectScope.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
 // component name TemplateFunction with the name for the new component.
-function projectScope(props) {
+function ProjectScope(props) {
   // Using hooks we're creating local state for a "heading" variable with
   // a default value of 'Functional Component'
   const store = useSelector((store) => store);
@@ -14,18 +14,109 @@ function projectScope(props) {
 
     return (
       <div className="projectScope">
-    <div className="headingClass">
-      <h2>{heading}</h2>
-      </div>
-      //create three template checkboxes with a heading that says "Preperation", and send their values to redux
-        <div className="checkboxClass">
-            <input type="checkbox" name="Preperation" value="Preperation" />
-            <label>Preperation</label>
-            </div>
-            </div>
-        
+        <h3>Preparation</h3>
 
-  );
+        <div className="checkboxClass">
+          <input
+            type="checkbox"
+            name="Preparation"
+            value="powerWash"
+          />
+          <label>
+            Power wash all paintable surfaces.
+          </label>
+          <br></br>
+          <input
+            type="checkbox"
+            name="Preparation"
+            value="Chip"
+          />
+          <label>
+            Scrape all loose and chipping
+            surfaces.
+          </label>
+          <br></br>
+          <input
+            type="checkbox"
+            name="Preparation"
+            value="Mildew"
+          />
+          <label>
+            Treat mildewed areas (if present) with
+            mildicide.
+          </label>
+          <br></br>
+          <br></br>
+        </div>
+        <div className="checkboxClass">
+          <h3>Warranty</h3>
+          <input
+            type="checkbox"
+            name="Warranty"
+            value="oneyear"
+          />
+          <label>
+            1-YEAR WARRANTY - Company warrants the
+            painting of exterior painted surfaces
+            for a period of one (1) years, per the
+            specific contracted scope of work, and
+            will repair newly painted areas if
+            peeling, blistering, flaking, or
+            coating separation occurs. This
+            warranty is limited to workmanship.
+            Our warranties cover painting defects,
+            but they do not cover areas of fading
+            and/or damage due to normal wear and
+            tear, abuse, weather, or accidental
+            damaging occurrences.
+          </label>
+          <br></br>
+          <br></br>
+          <input
+            type="checkbox"
+            name="Warranty"
+            value="threeYear"
+          />
+          <label>
+            3-YEAR WARRANTY - Company warrants the
+            painting of exterior painted surfaces
+            for a period of one (3) years, per the
+            specific contracted scope of work, and
+            will repair newly painted areas if
+            peeling, blistering, flaking, or
+            coating separation occurs. This
+            warranty is limited to workmanship.
+            Our warranties cover painting defects,
+            but they do not cover areas of fading
+            and/or damage due to normal wear and
+            tear, abuse, weather, or accidental
+            damaging occurrences.
+          </label>
+          <br></br>
+          <br></br>
+          <input
+            type="checkbox"
+            name="Warranty"
+            value="fiveYear"
+          />
+          <label>
+            5-YEAR WARRANTY - Company warrants the
+            painting of exterior painted surfaces
+            for a period of one (5) years, per the
+            specific contracted scope of work, and
+            will repair newly painted areas if
+            peeling, blistering, flaking, or
+            coating separation occurs. This
+            warranty is limited to workmanship.
+            Our warranties cover painting defects,
+            but they do not cover areas of fading
+            and/or damage due to normal wear and
+            tear, abuse, weather, or accidental
+            damaging occurrences.
+          </label>
+        </div>
+      </div>
+    );
 }
 
-export default projectScope;
+export default ProjectScope;
