98adce0d5b766c218e1b30c0530ac5ff3a243b15
Harrison Browning <harrisonbrowning1@gmail.com>
2022-08-27 16:26:19 -0500

    tied input changes to function that automatically updates reducer values

--- src/redux/reducers/projectDetails.reducer.js
+++ src/redux/reducers/projectDetails.reducer.js
@@ -1,4 +1,15 @@
-const projectDetailsReducer = (state = {}, action) => {
+const projectDetailsInput = {
+    property_type: '',
+    building_type: '',
+    project_interior: false,
+    project_exterior: false,
+    project_cabinetry: false,
+    project_specialFeature: false,
+    project_start_date: '',
+    project_end_date: '',
+  }
+
+const projectDetailsReducer = (state = projectDetailsInput, action) => {
     console.log('in projectDetails reducer', action.payload);
     switch (action.type) {
       case 'SET_PROJECT_DETAILS':
