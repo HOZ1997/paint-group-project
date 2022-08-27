5ff4f8827240f4df6ca693cdfd9d030a7821b12b
ahowes83 <a_howes83@yahoo.com>
2022-08-27 04:28:08 -0500

    in flagrante delicto (working of the proposal)

--- src/components/Proposal/Proposal.jsx
+++ src/components/Proposal/Proposal.jsx
@@ -17,7 +17,7 @@ function Proposal(props) {
 
   const history = useHistory();
 
-  const store = useSelector((store) => store.getProject);
+  const proposal = useSelector((store) => store.getProject[0]);
 
   const newProject = () => {
     history.push(``);
@@ -26,12 +26,28 @@ function Proposal(props) {
   return (
     <div className="container">
       <section id="frontPage">
-        {JSON.stringify(store)}
+        <img src="/images/PaintLogo.png" id="placeholderLogo"></img>
+        {/* {JSON.stringify(proposal)} */}
+        <h2>Proposal for {proposal.client_firstlast_name}</h2>
+        <hr></hr>
+        <br></br>
       </section>
       <section id="jobIntro">
 
+      <h3>Job # 00{proposal.project_job_number}</h3>
+        <ul>
+        <li>{proposal.project_propertytype_name}</li>
+        <li>{proposal.project_buildingtype_name}</li>
+      </ul>
+      <h4>Start Date: {proposal.project_startdate}</h4>
+      <h4>Start Date: {proposal.project_complete_specificdate}</h4>
+      <hr></hr>
+      <br></br>
       </section>
       <section id="clientDetails">
+        <h2>Client: {proposal.client_firstlast_name}</h2>
+        <br></br>
+        <p>{proposal.decision_emailaddress}</p>
 
       </section>
       <section id="proposalPhotos">
