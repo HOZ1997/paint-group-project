71bfa8e9fce88b3f35bb4ba0f4cf8110b56c06c9
Carlos Kelley <carloskelley@protonmail.com>
2022-08-30 00:03:09 -0500

    working on login page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -11,17 +11,9 @@ function LoginPage() {
       <img src="/images/Superstuct_logo.png" className="logo" />
       <LoginForm />
 
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
-          Register
-        </button>
-        </center>
+      {/* <center>
+        
+        </center> */}
         </div>
     </div>
     
