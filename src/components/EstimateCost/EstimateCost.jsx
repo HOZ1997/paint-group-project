fe9ac22212ab6d6edf5003fd274edc625c5c3eed
Jonathan Nguyen <njonathan990@gmail.com>
2022-08-31 01:31:52 -0500

    forgot 1 to save

--- src/components/EstimateCost/EstimateCost.jsx
+++ src/components/EstimateCost/EstimateCost.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {useHistory} from 'react-router-dom';
-import {Button} from 'react-bootstrap';
 
 function EstimateCost() {
   // ----- using this.state until connecting to database ----- //
