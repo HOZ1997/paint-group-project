da3dac32719a32ac478d08236177809dabb81fab
Carlos Kelley <carloskelley@protonmail.com>
2022-09-04 06:07:27 -0500

    finished styling proposal, moving on to work oder page

--- src/components/ProposalHeader/ProposalHeader.jsx
+++ src/components/ProposalHeader/ProposalHeader.jsx
@@ -3,34 +3,45 @@ import { Link } from "react-router-dom";
 import LogOutButton from "../LogOutButton/LogOutButton";
 import "./ProposalHeader.css";
 import { useSelector } from "react-redux";
-import {useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function ProposalHeader() {
   const location = useLocation();
   return (
     <div className="proposalHeader">
       <header>
-        {/* if /proposal, say Proposal */}
-        {location.pathname === "/proposal" && (
-          <h1 className="nav-title">Proposal</h1>
-        )}
-        {/* else if /projects, say Projects */}
+        {/* if /projects, say Projects */}
         {location.pathname === "/projects" && (
           <h1 className="nav-title">Projects</h1>
         )}
         {/* else if /ProjectInformationPage, say Project Information */}
         {location.pathname ===
           "/ProjectInformationPage" && (
-          <h1 className="nav-title" >
+          <h1 className="nav-title">
             Project Information
           </h1>
+          )}
+        {/* HARDCODED!!!!!!! */}
+        {location.pathname === "/Proposal/1" && (
+          <h1 className="nav-title">
+            Proposal
+          </h1>
         )}
-        {location.pathname ===
-          "/WorkOrder" && (
+        {/* HARDCODED!!!!!!! */}
+        {location.pathname === "/WorkOrder/1" && (
           <h1 className="nav-title">
             Work Order
           </h1>
         )}
+        {/* in any other case */}
+        {/* {location.pathname !== "/projects" &&
+          location.pathname !==
+            "/ProjectInformationPage" &&
+          location.pathname !== "/WorkOrder" && (
+            <h1 className="nav-title">
+              Proposal
+            </h1>
+          )} */}
       </header>
     </div>
   );
